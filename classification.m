1fefd25234a708c5e486f8f438e58d1a0d9bb3b4
Daniel Ribeiro Silva <drsilva@cyh100-b1.cyert.andrew.cmu.edu>
2014-12-03 01:55:55 -0500

    dim reduction

--- classification.m
+++ classification.m
@@ -7,32 +7,38 @@ CellSizes{2} = [7 7];
 CellSizes{3} = [4 4];
 BlockSize = [2 2];
 weights = [1 2 4];
+nDims = 30;
 
 %data
 folderNums = 1:10;
+allFeatures = [];
 
 for folder = folderNums
     folderName = getFolderName(folder);
     files = dir(['data/' folderName '/*.ppm']);
+    fprintf('Processing images for class %s...\n', folderName);
     for file = files'
         if file.name(1)=='.'
             continue
         end
         filePath = ['data/' folderName '/' file.name];
+        
+        %standard image format
         I = imread(filePath);
-        
-        
-        
+        I = rgb2gray(I);
+        I = imresize(I, [28 28]);
+
+        %get features
+        features = extractFeatures(I, CellSizes, BlockSize, weights);
+        allFeatures = [allFeatures; features];
     end 
 end
 
 
+%reduce dimension
+reduced_features = reduceDimension(allFeatures, nDims);
 
-I = rgb2gray(I);
 
-I = imresize(I, [28 28]);
-
-[features] = extractFeatures(I, CellSizes, BlockSize, weights);
 
 
 
--- reduceDimension.m
+++ reduceDimension.m
@@ -1,5 +1,8 @@
-function [reduced_features] = reduceDimension(features)
-
+function [reduced_features] = reduceDimension(features, nDims)
     
-
+    %covMat = cov(features');
+    %[reduced_features,~]=eigs(double(covMat),nDims);
+    
+    [~,scores] = princomp(zscore(features));
+    reduced_features = scores(:,1:nDims);
 end
