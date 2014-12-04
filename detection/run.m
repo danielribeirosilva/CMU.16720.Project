3be6708a4e76ac0ee6fdd4b6599601ff74ee3643
Daniel <danielsilva.ita@gmail.com>
2014-12-03 20:15:53 -0500

    debug msg

--- detection/run.m
+++ detection/run.m
@@ -1,8 +1,9 @@
 function [ ot ] = run( gt_train_file, gt_test_file, data_path )
-[ SVMModel ] = train( gt_train_file, data_path )
+[ SVMModel ] = train( gt_train_file, data_path );
 gt_test=load(gt_test_file);
 ot = [];
 for i=1:size(gt_test,1);
+    fprintf('Current test img: %i\n', i);
     test_file2 = sprintf('%05d.ppm', gt_test(i,1));
     test_file = sprintf('%s/%s', data_path, test_file2);
     I = imread(test_file);
